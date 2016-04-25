987ce101ca87505cf15c117a944c42afa0873ee5
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-25 15:31:00 +0200

    add task seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.create([
+  {title:"Seed the database", completed: true},
+  {title:"Create a controller for tasks"},
+  {title:"Create routing"},
+  {title:"print out some JSON I guess?"}
+  ])
+
+puts "Seeded #{Task.count} Tasks!"
